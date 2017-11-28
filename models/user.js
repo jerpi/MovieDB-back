7895b65b6d30de5b47586b80cf107874681cb963
jerpi <jeremy.piguel@gmail.com>
2017-11-28 19:55:47 +0100

    Added routes to match changes in MovieDB/front

---
+++ models/user.js
@@ -0,0 +1,23 @@
+class User {
+    _id; // mongo
+    username;
+    password; // crypted
+}
+
+class Comment {
+    _id; // mongo
+    user; // user id
+    date; //
+    film; // film id
+    title; //
+    content;
+    note;
+}
+
+class Film {
+
+}
+
+class Person {
+
+}
--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-
+router.get('/login', (req, res, next) => {
+    res.send(200, true);
 });
 
-router.post('/', (req, res, next) => {
-
+router.post('/login', (req, res, next) => {
+    res.send(200, true);
 });
+
+router.get('/logout', (req, res, next) => {
+    res.send(200, true);
+});
+
+router.post('/register', (req, res, next) => {
+    res.send(200, true);
+});
+
+router.get('/admin', (req, res, next) => {
+    res.send(200, true);
+});
+
 
 module.exports = router;
