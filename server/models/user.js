a60e93d3907beda452d7bb5710cb89ecd110a2e2
jerpi <jeremy.piguel@gmail.com>
2017-12-12 15:14:24 +0100

    Server files are now stored in server folder

---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+
+
+class Comment {
+    _id; // mongo
+    user; // user id
+    date; //
+    film; // film id
+    title; //
+    content;
+    note;
+}
+
+class Film {
+
+}
+
+class Person {
+
+}
